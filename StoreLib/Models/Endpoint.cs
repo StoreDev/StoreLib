9d821d4720e9d9108cb6add8649d6dd883e74e41
tunip3 <26260613+tunip3@users.noreply.github.com>
2020-08-26 16:44:56 +0100

    add more api endpoints

--- StoreLib/Models/Endpoint.cs
+++ StoreLib/Models/Endpoint.cs
@@ -19,6 +19,8 @@ namespace StoreLib.Models
     {
         public static readonly Uri FE3Delivery = new Uri("https://fe3.delivery.mp.microsoft.com/ClientWebService/client.asmx");
         public static readonly Uri FE3DeliverySecured = new Uri("https://fe3.delivery.mp.microsoft.com/ClientWebService/client.asmx/secured");
+        public static readonly Uri FE3CRDelivery = new Uri("https://fe3cr.delivery.mp.microsoft.com/ClientWebService/client.asmx");
+        public static readonly Uri FE3CRDeliverySecured = new Uri("https://fe3cr.delivery.mp.microsoft.com/ClientWebService/client.asmx/secured");
         public static readonly Uri DCATProd = new Uri("https://displaycatalog.mp.microsoft.com/v7.0/products/");
         public static readonly Uri DCATInt = new Uri("https://displaycatalog-int.mp.microsoft.com/v7.0/products/");
         public static readonly Uri DCATXbox = new Uri("https://xbox-displaycatalog.mp.microsoft.com/v7.0/products/");
@@ -27,6 +29,5 @@ namespace StoreLib.Models
         public static readonly Uri DCATOneP = new Uri("https://displaycatalog1p.mp.microsoft.com/v7.0/products/");
         public static readonly Uri DCATOnePInt = new Uri("https://displaycatalog1p-int.mp.microsoft.com/v7.0/products/");
         public static readonly Uri DisplayCatalogSearch = new Uri("https://displaycatalog.mp.microsoft.com/v7.0/productFamilies/autosuggest?market=US&languages=en-US&query=");
-        public static readonly Uri DisplayCatalogSearchInt = new Uri("https://displaycatalog-int.mp.microsoft.com/v7.0/productFamilies/autosuggest?market=US&languages=en-US&query=");
-    }
+        public static readonly Uri DisplayCatalogSearchInt = new Uri("https://displaycatalog-int.mp.microsoft.com/v7.0/productFamilies/autosuggest?market=US&languages=en-US&query=");    }
 }
