2dbde583d7c7742217b440ef5287c04976f51bc8
YoshiAsk <jjask7@gmail.com>
2020-11-03 00:56:46 -0600

    Added properties to make getting package information easier

--- StoreLib/Models/FE3.cs
+++ StoreLib/Models/FE3.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Xml.XPath;
 
 namespace StoreLib.Models
 {
-    public class PackageInstance
+    public class PackageInstance : IComparable<PackageInstance>
     {
         public string PackageMoniker;
         public Uri PackageUri;
@@ -19,6 +15,58 @@ namespace StoreLib.Models
             this.PackageType = packageType;
         }
 
-        
+        public string PackageFamily
+        {
+            get
+            {
+                try
+                {
+                    return PackageMoniker.Split('_')[0];
+                }
+                catch { return null; }
+            }
+        }
+
+        public Version Version
+        {
+            get
+            {
+                try
+                {
+                    return new Version(PackageMoniker.Split('_')[1]);
+                }
+                catch { return null; }
+            }
+        }
+
+        public string Architecture
+        {
+            get
+            {
+                try
+                {
+                    return PackageMoniker.Split('_')[2];
+                }
+                catch { return null; }
+            }
+        }
+
+        public string PublisherId
+        {
+            get
+            {
+                try
+                {
+                    var parts = PackageMoniker.Split('_');
+                    return parts[parts.Length - 1];
+                }
+                catch { return null; }
+            }
+        }
+
+        int IComparable<PackageInstance>.CompareTo(PackageInstance other)
+        {
+            return Version.CompareTo(other.Version);
+        }
     }
 }
